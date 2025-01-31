959636e8955baeedcdbf790c7ab626e3f8f366a4
Clifford <clifford.palmer@menloschool.org>
2025-01-31 00:41:59 -0800

    First Commit. Lots of thinking, not a ton of coding except a recursive method which finds all permutations of coins (not super helpful because it's super slow, and this problem is about combinations not permutations)

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -12,6 +12,25 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
-        return 0;
+        int combinations = 0;
+        for(int i = 0; i < coins.length; i++) {
+            combinations += countWaysRecur(target, coins, coins[i], 0);
+        }
+        return combinations;
+    }
+
+    public static long countWaysRecur(int target, int[] coins, int coinUsed, int debug) {
+        target = target - coinUsed;
+        if(target < 0) {
+            return 0;
+        }
+        if(target == 0){
+            return 1;
+        }
+        int combinations = 0;
+        for(int i = 0; i < coins.length; i++) {
+            combinations += countWaysRecur(target, coins, coins[i], debug + 1);
+        }
+        return combinations;
     }
 }
